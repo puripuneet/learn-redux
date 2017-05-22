fc645cbebf32d93ebd915eea8832e2a780a7f542
Puneet Puri <puneet@zikara.com>
2017-05-22 19:45:00 -0400

    Add subscribe and dev tools

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -18,11 +18,30 @@ var reducer = (state = stateDefault, action) => {
             return state;
     }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+store.subscribe(() => {
+    var state = store.getState();
+
+    document.getElementById('app').innerHTML = state.searchText;
+});
+
 console.log('currentState', store.getState());
 
 store.dispatch({
     type: 'CHANGE_SEARCH_TEXT',
     searchText: 'work'
 });
-console.log('searchText should be "work"', store.getState());
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'dog'
+});
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'Something else'
+});
