4056f185fea4dfb12e9a39391255a1bbce9caca8
Puneet Puri <puneet@zikara.com>
2017-05-24 18:04:32 -0400

    Convert actions to action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,60 +2,8 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var stateDefault = {
-    name: 'Anonymous',
-    hobbies: [],
-    movies: []
-};
-var nextHobbyId = 1;
-var nextMovieId = 1;
-var oldReducer = (state = stateDefault, action) => {
-    // state = state || {name: 'Anonymous'};
-
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return {
-                ...state,
-                name: action.name
-            };
-        case 'ADD_HOBBY':
-            return {
-                ...state,
-                hobbies: [
-                    ...state.hobbies,
-                    {
-                        id: nextHobbyId++,
-                        hobby: action.hobby
-                    }
-                ]
-            };
-        case 'REMOVE_HOBBY':
-            return {
-                ...state,
-                hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-            }
-        case 'ADD_MOVIE':
-            return {
-                ...state,
-                movies:[
-                    ...state.movies,
-                    {
-                        id: nextMovieId++,
-                        title: action.title,
-                        genre: action.genre
-                    }
-                ]
-            }
-        case 'REMOVE_MOVIE':
-            return {
-                ...state,
-                movies: state.movies.filter((movie) => movie.id !== action.id)
-            }
-        default:
-            return state;
-    }
-};
-
+// Name reducer and action generators
+// -----------------
 var nameReducer = (state = 'Anonymous', action) => {
     switch (action.type) {
         case 'CHANGE_NAME':
@@ -65,6 +13,16 @@ var nameReducer = (state = 'Anonymous', action) => {
     };
 };
 
+var changeName = (name) => {
+    return {
+        type: 'CHANGE_NAME',
+        name // same as name: name
+    };
+};
+
+// Hobbies reducer and action generators
+// -----------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_HOBBY':
@@ -82,6 +40,23 @@ var hobbiesReducer = (state = [], action) => {
     }
 };
 
+var addHobby = (hobby) => {
+    return {
+        type: 'ADD_HOBBY',
+        hobby
+    };
+};
+
+var removeHobby = (id) => {
+    return {
+        type: 'REMOVE_HOBBY',
+        id
+    };
+}
+
+// Movies reducer and action generators
+// -----------------
+var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
     switch (action.type) {
         case 'ADD_MOVIE':
@@ -98,6 +73,21 @@ var moviesReducer = (state = [], action) => {
         default:
             return state;
     }
+};
+
+var addMovie = (title, genre) => {
+    return {
+        type: 'ADD_MOVIE',
+        title,
+        genre
+    };
+};
+
+var removeMovie = (id) => {
+    return {
+        type: 'REMOVE_MOVIE',
+        id
+    };
 };
 
 var reducer = redux.combineReducers({
@@ -124,44 +114,14 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Andrew'
-});
+store.dispatch(changeName('Andrew'));
 
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Running'
-});
+store.dispatch(addHobby('Running'));
+store.dispatch(addHobby('Walking'));
+store.dispatch(removeHobby(2));
 
-store.dispatch({
-    type: 'ADD_HOBBY',
-    hobby: 'Walking'
-});
+store.dispatch(changeName('Emily'));
 
-store.dispatch({
-    type: 'REMOVE_HOBBY',
-    id: 2
-});
-
-store.dispatch({
-    type: 'CHANGE_NAME',
-    name: 'Emily'
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'Mad Max',
-    genre: 'Action'
-});
-
-store.dispatch({
-    type: 'ADD_MOVIE',
-    title: 'Alien',
-    genre: 'Science Fiction'
-});
-
-store.dispatch({
-    type: 'REMOVE_MOVIE',
-    id: 1
-});
+store.dispatch(addMovie('Mad Max', 'Action'));
+store.dispatch(addMovie('Alien', 'Science Fiction'));
+store.dispatch(removeMovie(1));
